d889ed4ec9b4af862f81cf1bfed4dcccd3744c9c
suraj <suraj@monetizemore.com>
2021-11-11 09:02:56 +0530

    Updated templates

--- build/dynomodb_restore.py
+++ build/dynomodb_restore.py
@@ -12,7 +12,6 @@ def load_items(items, db_table_name):
 
 
 def pick_json_files(db_file, db_table_name):
-
     with open(db_file) as json_file:
         items = json.load(json_file, parse_float=Decimal)
     load_items(items, db_table_name)
